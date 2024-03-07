fc9207be98ac7bffd9df53c92be0d29dc4f1c77d
Andreas Aanestad <andreas.aanestad@gmail.com>
2024-03-07 21:32:22 +0100

    asd

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 2000;
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${port}`);
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
