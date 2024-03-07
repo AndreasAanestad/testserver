96700fcd159cb63d00774244dec531fe7342a5bf
Andreas Aanestad <andreas.aanestad@gmail.com>
2024-03-07 21:19:56 +0100

    0.0.0.0

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 2000;
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
