5e0a4becf5152da94cacf93d7d614101b0efe2eb
Andreas Aanestad <andreas.aanestad@gmail.com>
2024-03-07 21:07:25 +0100

    first commit

---
+++ public/script.js
@@ -0,0 +1,11 @@
+document.getElementById('btn1').addEventListener('click', function() {
+    alert('Button 1 was clicked!');
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+    alert('Button 2 was clicked!');
+});
+
+document.getElementById('btn3').addEventListener('click', function() {
+    alert('Button 3 was clicked!');
+});
